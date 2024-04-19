f5ca8d82be53516c4e69b56e2a676befa2c83366
Yunasawa <yunasawa100@gmail.com>
2024-04-20 02:10:15 +0700

    A

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,44 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using YNL.Extension.Method;
+using YNL.Tools.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
-    public float dashDistance;
-    float moveX;
-    float moveY;
-    private Vector3 moveDirection;
-    bool isDashButtonDown = false;
-    Rigidbody2D rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    public float _speed = 0.05f;
+    public float _dashDistance;
+    private Vector2 _movingInput;
 
-    // Update is called once per frame
+    private Coroutine _dashingCoroutine;
+
     void Update()
     {
-        moveX = Input.GetAxisRaw("Horizontal");
-        moveY = Input.GetAxisRaw("Vertical");
-        moveDirection = new Vector3(moveX, moveY).normalized;
-        if (Input.GetKeyDown(KeyCode.Space))
+        _movingInput = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.position += ToVector3(_movingInput * _speed.Oscillate(120));
+
+        if (MInput.PressSpace())
         {
-            isDashButtonDown = true;
+            if (!_dashingCoroutine.IsNull()) StopCoroutine(_dashingCoroutine);
+            _dashingCoroutine = StartCoroutine(Dashing(transform.position + ToVector3(_movingInput.normalized * _dashDistance), 1));
         }
 
-
     }
-    public void FixedUpdate()
+
+    public Vector3 ToVector3(Vector2 input) => new Vector3(input.x, input.y, 0);
+
+    public IEnumerator Dashing(Vector2 destination, float duration, TweenType tween = TweenType.ExponentialInterpolation)
     {
-        rb.velocity = ToVector2(moveDirection) * speed;
-        if (isDashButtonDown)
+        float elapsedTime = 0f;
+
+        Vector3 startPosition = transform.position;
+
+        while (elapsedTime < duration - 0.85f)
         {
-            rb.MovePosition(transform.position + moveDirection * dashDistance);
-            isDashButtonDown = false;
+            elapsedTime += Time.deltaTime;
+            float normalizedTime = Mathf.Clamp01(elapsedTime / duration);
+
+            if (tween == TweenType.ExponentialInterpolation) transform.localPosition = Vector2.Lerp(transform.localPosition, destination, normalizedTime);
+            if (tween == TweenType.LinearInterpolation) transform.localPosition = Vector2.Lerp(startPosition, destination, normalizedTime);
+
+            yield return null;
         }
     }
-    public Vector2 ToVector2(Vector3 input)
-    => new Vector2(input.x, input.y);
 }
